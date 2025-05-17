d4da90fb0e0bd1096f4db343e062090d84f663e8
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 17:55:47 -0400

    styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,17 @@ import { asImageSrc } from "@prismicio/client";
 import { createClient } from "@/prismicio";
 import { Logo } from "@/components/Logo";
 import { Bounded } from "./Bounded";
+import { FooterPhysics } from "./FooterPhysics";
 
 export async function Footer() {
   const client = createClient()
   const settings = await client.getSingle('settings')
+
+
+  const boardTextureURLs = settings.data.footer_skateboards
+    .map((item) => asImageSrc(item.skateboard, { h: 600 }))
+    .filter((url): url is string => Boolean(url));
+
 
   return (
     <footer className="bg-texture bg-zinc-900 text-white overflow-hidden">
@@ -17,6 +24,9 @@ export async function Footer() {
         <PrismicNextImage field={settings.data.footer_image} alt='' fill className="object-cover" width={1200} />
 
       {/* Physics Boards */}
+      <FooterPhysics boardTextureURLs={boardTextureURLs}
+        className="absolute inset-0 overflow-hidden"
+      />
       <Logo  className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28"/>
       </div>
 
