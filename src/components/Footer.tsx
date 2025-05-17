630d53ce2e9ad48d764ab457521ebddf67afe4bb
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 17:41:14 -0400

    adds footer image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import React from "react";
+import { asImageSrc } from "@prismicio/client";
+
+import { createClient } from "@/prismicio";
+import { Logo } from "@/components/Logo";
+import { Bounded } from "./Bounded";
+
 export async function Footer() {
+  const client = createClient()
+  const settings = await client.getSingle('settings')
+
   return (
-    <footer>
-      Footer
+    <footer className="bg-texture bg-zinc-900 text-white overflow-hidden">
+      <div className="relative h-[75vh] ~p-10/16 md:aspect-auto">
+        <PrismicNextImage field={settings.data.footer_image} alt='' fill className="object-cover" width={1200} />
+      {/* Image */}
+      {/* Physics Boards */}
+      </div>
+
+      {/* List of links */}
     </footer>
   )
 
