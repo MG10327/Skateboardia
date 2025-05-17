7780853354ce6b08c25a9c5b630b56de4f0c472b
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 14:16:40 -0400

    update setup for skateboard product

--- src/slices/ProductGrid/SkateboardProduct.tsx
+++ src/slices/ProductGrid/SkateboardProduct.tsx
@@ -1,5 +1,7 @@
-import { Content, createClient } from '@prismicio/client'
+import { Content, createClient, isFilled } from '@prismicio/client'
+import { PrismicNextImage } from '@prismicio/next'
 import React from 'react'
+import { FaStar } from 'react-icons/fa6'
 
 type Props = {
     id: string
@@ -9,8 +11,23 @@ export async function SkateboardProduct({id}: Props) {
     const client = createClient()
     const product = await client.getByID<Content.SkateboardDocument>(id)
 
+    const price = isFilled.number(product.data.price) ? `${(product.data.price / 100).toFixed(2)}` : 'Price Not available'
+
     return (
-    <div>SkateboardProduct</div>
+
+    <div>
+        <div className="flex items-center justicy-between ~text-sm/2xl">
+            <span>{price}</span>
+            <span className="inline-flex items-center gap-1">
+                <FaStar className="text-yellow-400" /> 37
+            </span>
+        </div>
+
+        <div className="-mb-1 overflow-hidden py-4">
+            <PrismicNextImage alt="" field={product.data.image} width={150} />
+            
+        </div>
+    </div>
   )
 }
 
