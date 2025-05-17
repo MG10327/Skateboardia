ab6ec7850cd8e3c1eaf74f93ec5a70c4eda4ec6d
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 16:29:57 -0400

    wrap up parallaxes

--- src/slices/TextAndImage/index.tsx
+++ src/slices/TextAndImage/index.tsx
@@ -1,56 +1,80 @@
-import { Bounded } from '@/components/Bounded'
-import { ButtonLink } from '@/components/ButtonLink'
-import { Heading } from '@/components/Heading'
-import { Content } from '@prismicio/client'
-import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
-import { PrismicRichText, SliceComponentProps } from '@prismicio/react'
-import clsx from 'clsx'
-import React from 'react'
+import { Content } from "@prismicio/client";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import clsx from "clsx";
 
-export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>
+import { Bounded } from "@/components/Bounded";
+import { ButtonLink } from "@/components/ButtonLink";
+import { Heading } from "@/components/Heading";
+import { SlideIn } from "@/components/SlideIn";
+import { ParallaxImage } from "./ParallaxImage";
 
+declare module "react" {
+  interface CSSProperties {
+    "--index"?: number;
+  }
+}
 
+/**
+ * Props for `TextAndImage`.
+ */
+export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>;
 
-const index = ({slice}: TextAndImageProps): JSX.Element => {
-  const theme = slice.primary.theme // easier to reference like this
-
+/**
+ * Component for "TextAndImage" Slices.
+ */
+const TextAndImage = ({ slice, index }: TextAndImageProps): JSX.Element => {
+  const theme = slice.primary.theme;
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={clsx(
-        slice.primary.theme === 'Blue' && 'bg-texture bg-brand-blue text-white',
-        slice.primary.theme === 'Orange' && 'bg-texture bg-brand-orange text-white',
-        slice.primary.theme === 'Navy' && 'bg-texture bg-brand-Navy ',
-        slice.primary.theme === 'Lime' && 'bg-texture bg-brand-lime',
+        "sticky top-[calc(var(--index)*2rem)]",
+        theme === "Blue" && "bg-texture bg-brand-blue text-white",
+        theme === "Orange" && "bg-texture bg-brand-orange text-white",
+        theme === "Navy" && "bg-texture bg-brand-navy text-white",
+        theme === "Lime" && "bg-texture bg-brand-lime"
       )}
+      style={{ "--index": index }}
     >
-
       <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
-        <div className={clsx(  "flex flex-col items-center gap-8 text-center md:items-start md:text-left",
-            slice.variation === "imageOnLeft" && "md:order-2"  )} >
-
-          <Heading size='lg' as="h2" >
-            <PrismicRichText field={slice.primary.heading}/>
-          </Heading>
-
-          <div className="max-w-md text-lg leading-relaxed">
-            <PrismicRichText field={slice.primary.body}/>
-          </div>
-
-          <ButtonLink field={slice.primary.button} color={theme === 'Lime' ? 'orange' : 'lime'}>
-            {slice.primary.button.text}
-          </ButtonLink>
+        <div
+          className={clsx(
+            "flex flex-col items-center gap-8 text-center md:items-start md:text-left",
+            slice.variation === "imageOnLeft" && "md:order-2"
+          )}
+        >
+          <SlideIn>
+            <Heading size="lg" as="h2">
+              <PrismicText field={slice.primary.heading} />
+            </Heading>
+          </SlideIn>
+          <SlideIn>
+            <div className="max-w-md text-lg leading-relaxed">
+              <PrismicRichText field={slice.primary.body} />
+            </div>
+          </SlideIn>
+          <SlideIn>
+            <ButtonLink
+              field={slice.primary.button}
+              color={theme === "Lime" ? "orange" : "lime"}
+            >
+              {slice.primary.button.text}
+            </ButtonLink>
+          </SlideIn>
         </div>
 
-
-          <PrismicNextImage field={slice.primary.background_image}/>
-          <PrismicNextImage field={slice.primary.foreground_image}/>
-
+        <ParallaxImage
+          foregroundImage={slice.primary.foreground_image}
+          backgroundImage={slice.primary.background_image}
+        />
       </div>
-
     </Bounded>
-  )
-}
+  );
+};
 
-export default index
+export default TextAndImage;
