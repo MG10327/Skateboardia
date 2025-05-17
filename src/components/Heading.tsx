9b4f230328855e6cd67c42d6913edc8e83cf157c
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 12:16:23 -0400

    start basic header setup

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,33 @@
-export function Heading() {
-  return <div />;
+import clsx from "clsx";
+
+type HeadingProps = {
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  size?: "xl" | "lg" | "md" | "sm" | "xs";
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function Heading({
+  as: Comp = "h1",
+  className,
+  children,
+  size = "lg",
+}: HeadingProps) {
+  return (
+    <Comp
+      className={clsx(
+        "font-sans uppercase",
+        size === "xl" && "~text-4xl/8xl",
+        size === "lg" && "~text-4xl/7xl",
+        size === "md" && "~text-3xl/5xl",
+        size === "sm" && "~text-2xl/4xl",
+        size === "xs" && "~text-lg/xl",
+        className,
+      )}
+    >
+      {children}
+    </Comp>
+  );
 }
+
+// responsive heading that can be  one of six heading types
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { asImageSrc, Content } from "@prismicio/client";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import { PrismicNextLink } from '@prismicio/next';
+import { Bounded } from '@/components/Bounded';
+import { Heading } from '@/components/Heading';
 
 
-const index = () => {
+export type HeroProps = SliceComponentProps<Content.HeroSlice>
+
+
+const Hero = ({slice}: HeroProps) => {
   return (
-    <div>index</div>
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
+    >
+      <div className="grid absolute inset-0 mx-auto mt-24 max-w-6xl grid-rows-2 place-items-end px-6 ~py-10/16">
+
+      </div>
+      <Heading size="lg" className="relative max-w-2xl place-self-start">
+        <PrismicRichText field={slice.primary.heading} />
+      </Heading>
+
+      <div className="flex relative w-full flex-col items-center justify-between ~gap-2/4 lg:flex-row">
+      <div className="max-w-[45ch] font-semibold ~text-lg-xl">
+
+      </div>
+        <PrismicRichText field={slice.primary.body} />
+        <PrismicNextLink field={slice.primary.button} />
+      </div>
+
+    </Bounded>
   )
 }
 
-export default index
+export default Hero
