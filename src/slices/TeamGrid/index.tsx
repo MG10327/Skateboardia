47c11ffbf508dbfb11e47d2e1dd5331d2401b3cf
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 17:28:36 -0400

    update team section

--- src/slices/TeamGrid/index.tsx
+++ src/slices/TeamGrid/index.tsx
@@ -1,8 +1,11 @@
+import { Bounded } from "@/components/Bounded";
 import { Heading } from "@/components/Heading";
 import { createClient } from "@/prismicio";
 import { Content } from "@prismicio/client";
 import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import React from "react";
 import { Skater } from "./Skater";
+import { SlideIn } from "@/components/SlideIn";
 
 /**
  * Props for `TeamGrid`.
@@ -17,23 +20,28 @@ const TeamGrid = async ({ slice }: TeamGridProps): Promise<JSX.Element> => {
   const skaters = await client.getAllByType("skater");
 
   return (
-    <div
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="bg-texture bg-brand-navy px-6 py-16"
+      className="bg-texture bg-brand-navy"
     >
-      <Heading as="h2" size="lg" className="mb-8 text-center text-white">
-        <PrismicText field={slice.primary.heading} />
-      </Heading>
+      <SlideIn>
+        <Heading as="h2" size="lg" className="mb-8 text-center text-white">
+          <PrismicText field={slice.primary.heading} />
+        </Heading>
+      </SlideIn>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-        {skaters.map(
-          (skater, index) =>
-            skater.data.first_name && (
-              <Skater key={index} index={index} skater={skater} />
-            )
-        )}
+        {skaters.map((skater, index) => (
+          <React.Fragment key={index}>
+            {skater.data.first_name && (
+              <SlideIn>
+                <Skater index={index} skater={skater} />
+              </SlideIn>
+            )}
+          </React.Fragment>
+        ))}
       </div>
-    </div>
+    </Bounded>
   );
 };
 
