684950c7f7cd40d0f3ab8bb8af5afaa72d70282c
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-17 22:29:00 -0400

    Cleanup all main files

--- src/slices/ProductGrid/index.tsx
+++ src/slices/ProductGrid/index.tsx
@@ -10,23 +10,31 @@ import { Heading } from "@/components/Heading";
 import { SkateboardProduct } from "./SkateboardProduct";
 import { SlideIn } from "@/components/SlideIn";
 
+/**
+ * Props for `ProductGrid`.
+ */
+export type ProductGridProps = SliceComponentProps<Content.ProductGridSlice>;
 
-export type ProductGridProps = SliceComponentProps<Content.ProductGridSlice>
-
-const ProductGrid = ({slice} : ProductGridProps) => {
+/**
+ * Component for "ProductGrid" Slices.
+ */
+const ProductGrid = ({ slice }: ProductGridProps): JSX.Element => {
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-gray"
     >
-      <Heading className='text-center bg-grand-gray' >
-        <PrismicRichText field={slice.primary.heading} />
-      </Heading>
-
-      <div className="text-center ~mb-6/10">
-        <PrismicRichText field={slice.primary.body} />
-      </div>
-
+      <SlideIn>
+        <Heading className="text-center ~mb-4/6" as="h2">
+          <PrismicText field={slice.primary.heading} />
+        </Heading>
+      </SlideIn>
+      <SlideIn>
+        <div className="text-center ~mb-6/10">
+          <PrismicRichText field={slice.primary.body} />
+        </div>
+      </SlideIn>
       <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         {slice.primary.product.map(
           ({ skateboard }) =>
@@ -35,10 +43,8 @@ const ProductGrid = ({slice} : ProductGridProps) => {
             )
         )}
       </div>
-
-
     </Bounded>
-  )
-}
+  );
+};
 
-export default ProductGrid
+export default ProductGrid;
