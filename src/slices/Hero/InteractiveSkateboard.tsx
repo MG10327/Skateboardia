ab797a55a0031f3868a7fbf737533ad5b2ae478c
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-18 12:24:37 -0400

    update animation build

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -1,255 +1,194 @@
-"use client";
+'use client' // client side rendering. Required for GSAP, three Drei + three fiber 3d work, use State use Ref, etc.
 
-import * as THREE from "three";
-import { Skateboard } from "@/components/Skateboard";
-import { ContactShadows, Environment, Html } from "@react-three/drei";
-import { Canvas, ThreeEvent, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useRef, useState } from "react";
-import gsap from "gsap";
-import { Hotspot } from "./Hotspot";
-import { WavyPaths } from "./WavyPaths";
+import { Skateboard } from '@/components/Skateboard'
+import { ContactShadows, Environment, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import * as THREE from 'three'
+import { useRef } from 'react'
+import { MeshStandardNodeMaterial } from 'three/webgpu'
+import { ThreeEvent } from '@react-three/fiber'
+import gsap from 'gsap'
 
-const INITIAL_CAMERA_POSITION = [1.5, 1, 1.4] as const;
+type Props = {}
 
-type Props = {
-  deckTextureURL: string;
-  wheelTextureURL: string;
-  truckColor: string;
-  boltColor: string;
-};
+export default function InteractiveSkateboard({ defaultTextureURL, wheelTextureURL, truckColor, boltColor}: Props) {
+    const containerRef = useRef<THREE.Group>(null)
+    const originRef = useRef<THREE.Group>(null) // this is for the front board trick so we can rotate from the center mid way through.
 
-export function InteractiveSkateboard({
-  deckTextureURL,
-  wheelTextureURL,
-  truckColor,
-  boltColor,
-}: Props) {
-  return (
-    <div className="absolute inset-0 z-10 flex items-center justify-center">
-      <Canvas
-        className="min-h-[60rem] w-full"
-        camera={{ position: INITIAL_CAMERA_POSITION, fov: 55 }}
-      >
-        <Suspense>
-          <Scene
-            deckTextureURL={deckTextureURL}
-            wheelTextureURL={wheelTextureURL}
-            truckColor={truckColor}
-            boltColor={boltColor}
-          />
-        </Suspense>
-      </Canvas>
+
+    const onClick = (event: ThreeEvent<MouseEvent>) =>{
+        event.stopPropagation()
+
+        const board = containerRef.current
+        const origin = originRef.current
+
+        if(!board || !origin) return // stops if there is no board
+
+        const {name} = event.object
+
+        // now we start our animations
+        if(name === 'back'){
+            ollie(board)
+        } else if (name === 'middle') {
+            // if the user clicks on the middle of the board we'll activate our kick flip animation
+            kickflip(board)
+        } else if (name === 'front') {
+            frontSide360(board, origin)
+        }
+
+
+
+    }
+
+    function ollie(board: THREE.Group){
+
+        jumpBoard(board) // sends the board into the air first and then does the trick part
+        // Tilts the board to mimick skateboard tricks. Tilts the board back and then straightens it bit by bit
+        gsap.timeline()
+        .to(board.rotation, { // animates to tilted back board to simulate an ollie.
+            x: -.6,
+            duration: .26,
+            ease: 'none'
+        })
+        .to(board.rotation, { // animates to slightly less tilted board
+            x: .4,
+            duration: .82,
+            ease: 'none'
+        })
+        .to(board.rotation, { // then animates to straight board
+            x: 0,
+            duration: .12,
+            ease: 'none'
+        })
+    }
+
+
+    // now we make our kickflip rotation animation
+    function kickflip(board: THREE.Group){
+        // Tilts the board to mimick skateboard tricks. Tilts the board back and then straightens it bit by bit
+        jumpBoard(board) /// sends the board into the air first then does the trick part after
+
+        gsap.timeline()
+        .to(board.rotation, { // animates to tilted back board to simulate an ollie.
+            x: -.6,
+            duration: .26,
+            ease: 'none'
+        })
+        .to(board.rotation, { // animates to slightly less tilted board
+            x: .4,
+            duration: .82,
+            ease: 'none'
+        })
+        .to(board.rotation, {
+            z: `+=${Math.PI * 2}`,
+            duration: .78,
+            ease: 'none'
+        }, .3) // this is the position argument after all the other stuff. this means after X chained of time this animation will start so they don't all start at once
+        .to(board.rotation, { // then animates to straight board
+            x: 0,
+            duration: .12,
+            ease: 'none'
+        })
+    }
+
+
+    function frontSide360(board: THREE.Group){
+        jumpBoard(board) // sends the board into the air first then starts the trick
+
+        // Tilts the board to mimick skateboard tricks. Tilts the board back and then straightens it bit by bit
+        // We start with our kickflip animation as a base and add to it.
+        gsap.timeline()
+        .to(board.rotation, { // animates to tilted back board to simulate an ollie.
+            x: -.6,
+            duration: .26,
+            ease: 'none'
+        })
+        .to(board.rotation, { // animates to slightly less tilted board
+            x: .4,
+            duration: .82,
+            ease: 'none'
+        })
+        .to(origin.rotation, {
+            z: `+=${Math.PI * 2}`, // 360 degree rotation
+            duration: .77,
+            ease: 'none'
+        }, .3) // this is the position argument after all the other stuff. this means after X chained of time this animation will start so they don't all start at once
+        .to(board.rotation, { // then animates to straight board
+            x: 0,
+            duration: .12,
+            ease: 'none'
+        })
+    }
+
+
+    // All boards will go up in the air and come down at the same rate
+    function jumpBoard(board: THREE.Group){
+                // Sends the board up and down
+        gsap.timeline() // starts from the elements current state and animates to what we put in here
+        .to(board.position, {
+            y: .8,
+            duration: .51,
+            ease: 'power2.out',
+            delay: .26
+        }) // then to here
+        .to(board.position, {
+            y: 0,
+            duration: .43,
+            ease: 'power2.in'
+        })
+
+    }
+
+
+    return (
+    <div className='absolute inset-0 z-10 flex items-center justify-center'>
+        <Canvas className='min-h-[60rem] w-full' camera={{position: [1.5, 1, 1.4], fov: 55}}>
+            <Suspense>
+                <Scene />
+            </Suspense>
+        </Canvas>
     </div>
-  );
+  )
 }
 
-function Scene({
-  deckTextureURL,
-  wheelTextureURL,
-  truckColor,
-  boltColor,
-}: Props) {
-  const containerRef = useRef<THREE.Group>(null);
-  const originRef = useRef<THREE.Group>(null);
+function Scene(){
+    return (
+        <group>
+            <OrbitControls />
+            <Environment files='hdr/warehouse-256.hdr' background />
+            <group ref={originRef}>
+                <group ref={containerRef}>
+                    <group position={[0, -0.086, 0.635]}>
 
-  const [animating, setAnimating] = useState(false);
-  const [showHotspot, setShowHotspot] = useState({
-    front: true,
-    middle: true,
-    back: true,
-  });
+                        <Skateboard
+                            wheelTextureURLs={[wheelTextureURL]}
+                            wheelTextureURL={wheelTextureURL}
+                            deckTextureURLs={[deckTextureURL]}
+                            deckTextureURL={deckTextureURL}
+                            truckColor={truckColor}
+                            boltColor={boltColor}
+                            constantWheelSpin
+                        />
+                        <mesh onClick={onClick} position={[0, 0.27, 0.9]} name='front'>
+                            <boxGeometry args={[0.6, 0.2, .58]} />
+                            <meshStandardMaterial visible={false} />
+                        </mesh>
 
-  const { camera } = useThree();
+                        <mesh onClick={onClick} position={[0, 0.27, 0]} name='middle'>
+                            <boxGeometry args={[0.6, 0.1, 1.2]} />
+                            <meshStandardMaterial visible={false} />
+                        </mesh>
 
-  useEffect(() => {
-    if (!containerRef.current || !originRef.current) return;
+                        <mesh onClick={onClick} position={[0, 0.27, -0.9]} name='back'>
+                            <boxGeometry args={[0.6, 0.2, .58]} />
+                            <meshStandardMaterial visible={false} />
+                        </mesh>
 
-    gsap.to(containerRef.current.position, {
-      x: 0.2,
-      duration: 3,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut",
-    });
-
-    gsap.to(originRef.current.rotation, {
-      y: Math.PI / 64,
-      duration: 3,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut",
-    });
-  }, []);
-
-  useEffect(() => {
-    camera.lookAt(new THREE.Vector3(-0.2, 0.15, 0));
-
-    setZoom();
-
-    window.addEventListener("resize", setZoom);
-
-    function setZoom() {
-      const scale = Math.max(Math.min(1000 / window.innerWidth, 2.2), 1);
-
-      camera.position.x = INITIAL_CAMERA_POSITION[0] * scale;
-      camera.position.y = INITIAL_CAMERA_POSITION[1] * scale;
-      camera.position.z = INITIAL_CAMERA_POSITION[2] * scale;
-    }
-
-    return () => window.removeEventListener("resize", setZoom);
-  }, [camera]);
-
-  function onClick(event: ThreeEvent<MouseEvent>) {
-    event.stopPropagation();
-
-    const board = containerRef.current;
-    const origin = originRef.current;
-
-    if (!board || !origin || animating) return;
-
-    const { name } = event.object;
-
-    setShowHotspot((current) => ({ ...current, [name]: false }));
-
-    if (name === "back") {
-      ollie(board);
-    } else if (name === "middle") {
-      kickflip(board);
-    } else if (name === "front") {
-      frontside360(board, origin);
-    }
-  }
-
-  function ollie(board: THREE.Group) {
-    jumpBoard(board);
-
-    gsap
-      .timeline()
-      .to(board.rotation, { x: -0.6, duration: 0.26, ease: "none" })
-      .to(board.rotation, { x: 0.4, duration: 0.82, ease: "power2.in" })
-      .to(board.rotation, { x: 0, duration: 0.12, ease: "none" });
-  }
-
-  function kickflip(board: THREE.Group) {
-    jumpBoard(board);
-
-    gsap
-      .timeline()
-      .to(board.rotation, { x: -0.6, duration: 0.26, ease: "none" })
-      .to(board.rotation, { x: 0.4, duration: 0.82, ease: "power2.in" })
-      .to(
-        board.rotation,
-        {
-          z: `+=${Math.PI * 2}`,
-          duration: 0.78,
-          ease: "none",
-        },
-        0.3
-      )
-      .to(board.rotation, { x: 0, duration: 0.12, ease: "none" });
-  }
-
-  function frontside360(board: THREE.Group, origin: THREE.Group) {
-    jumpBoard(board);
-
-    gsap
-      .timeline()
-      .to(board.rotation, { x: -0.6, duration: 0.26, ease: "none" })
-      .to(board.rotation, { x: 0.4, duration: 0.82, ease: "power2.in" })
-      .to(
-        origin.rotation,
-        {
-          y: `+=${Math.PI * 2}`,
-          duration: 0.77,
-          ease: "none",
-        },
-        0.3
-      )
-      .to(board.rotation, { x: 0, duration: 0.14, ease: "none" });
-  }
-
-  function jumpBoard(board: THREE.Group) {
-    setAnimating(true);
-
-    gsap
-      .timeline({ onComplete: () => setAnimating(false) })
-      .to(board.position, {
-        y: 0.8,
-        duration: 0.51,
-        ease: "power2.out",
-        delay: 0.26,
-      })
-      .to(board.position, {
-        y: 0,
-        duration: 0.43,
-        ease: "power2.in",
-      });
-  }
-
-  return (
-    <group>
-      <Environment files={"/hdr/warehouse-256.hdr"} />
-      <group ref={originRef}>
-        <group ref={containerRef} position={[-0.25, 0, -0.635]}>
-          <group position={[0, -0.086, 0.635]}>
-            <Skateboard
-              wheelTextureURLs={[wheelTextureURL]}
-              wheelTextureURL={wheelTextureURL}
-              deckTextureURLs={[deckTextureURL]}
-              deckTextureURL={deckTextureURL}
-              truckColor={truckColor}
-              boltColor={boltColor}
-              constantWheelSpin
-            />
-
-            <Hotspot
-              isVisible={!animating && showHotspot.front}
-              position={[0, 0.38, 1]}
-              color="#B8FC39"
-            />
-
-            <mesh position={[0, 0.27, 0.9]} name="front" onClick={onClick}>
-              <boxGeometry args={[0.6, 0.2, 0.58]} />
-              <meshStandardMaterial visible={false} />
-            </mesh>
-
-            <Hotspot
-              isVisible={!animating && showHotspot.middle}
-              position={[0, 0.33, 0]}
-              color="#FF7A51"
-            />
-            <mesh position={[0, 0.27, 0]} name="middle" onClick={onClick}>
-              <boxGeometry args={[0.6, 0.1, 1.2]} />
-              <meshStandardMaterial visible={false} />
-            </mesh>
-
-            <Hotspot
-              isVisible={!animating && showHotspot.back}
-              position={[0, 0.35, -0.9]}
-              color="#46ACFA"
-            />
-            <mesh position={[0, 0.27, -0.9]} name="back" onClick={onClick}>
-              <boxGeometry args={[0.6, 0.2, 0.58]} />
-              <meshStandardMaterial visible={false} />
-            </mesh>
-          </group>
+                        <ContactShadows opacity={0.6} />
+                    </group>
+                </group>
+            </group>
         </group>
-      </group>
-      <ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
-      <group
-        rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
-        position={[0, -0.09, -0.5]}
-        scale={[0.2, 0.2, 0.2]}
-      >
-        <Html
-          wrapperClass="pointer-events-none"
-          transform
-          zIndexRange={[1, 0]}
-          occlude="blending"
-        >
-          <WavyPaths />
-        </Html>
-      </group>
-    </group>
-  );
+    )
 }
